c10a3e7a22d61ef7da5a4854750b63869f9657ed
SayQuit <501241011@qq.com>
2022-08-17 20:30:16 +0800

    商品详情完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -10,6 +10,15 @@ Page({
     swiperList:[]
   },
 
+  priview:function(e){
+    // console.log(e);
+    var u=this.data.swiperList[e.currentTarget.dataset.index].pics_big
+    // console.log(u);
+    wx.previewImage({
+      urls: [u],
+    })
+  },
+
   getdetailList:function(){
     var link='https://api-hmugo-web.itheima.net/api/public/v1/goods/detail?goods_id='+this.data.id;
     // console.log(link);
@@ -31,6 +40,29 @@ Page({
     }),
     this.getdetailList()
   },
+  addShoppingCar:function(){
+    var shoppingcar=wx.getStorageSync('ShoppingCar')||[]
+    var index=shoppingcar.findIndex(v=>v.goods_id==this.data.detailList.goods_id)
+    // console.log(index);
+    console.log(shoppingcar);
+    // console.log(this.data.detailList);
+    if(index==-1){
+      shoppingcar.push(this.data.detailList);
+      wx.setStorageSync('ShoppingCar',shoppingcar)
+      wx.showToast({
+        title: '加入成功',
+        mask:true
+      })
+    }
+    else{
+      wx.showToast({
+      title: '商品已存在',
+      mask:true
+    })
+    }
+
+
+  }
 
   
 })
