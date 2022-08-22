ae84feacd456818afd7e3b61859233b845e99d83
SayQuit <501241011@qq.com>
2022-08-22 15:50:56 +0800

    用户页面样式

--- pages/user/index.js
+++ pages/user/index.js
@@ -2,7 +2,51 @@
 Page({
   data: {
     userInfo:[],
-    isLogin:false
+    isLogin:false,
+    nav:[
+      {
+        id:0,
+        name:'收藏的店铺',
+        num:0
+      },
+      {
+        id:1,
+        name:'收藏的商品',
+        num:0
+      },
+      {
+        id:2,
+        name:'关注的商品',
+        num:0
+      },
+      {
+        id:3,
+        name:'我的足迹',
+        num:0
+      }
+    ],
+    order:[
+      {
+        id:0,
+        name:'全部订单',
+        img:''
+      },
+      {
+        id:1,
+        name:'待付款',
+        img:''
+      },
+      {
+        id:2,
+        name:'待收货',
+        img:''
+      },
+      {
+        id:3,
+        name:'退款/退货',
+        img:''
+      }
+    ]
   },
   getUserInfo:function(){
 
